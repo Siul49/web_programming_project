139f0d454dbdc0c4f1528e32ab72ccae8e0015cd
ArgX2 <pkpk0514@gmail.com>
2025-04-03 15:35:23 +0900

    04/03 웹개발 스터디

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./Time_Table/*.html'],
+  content: ['./Time_Table/*.html', './index.html', './Mat_zip.html'],
   theme: {
     extend: {},
   },
