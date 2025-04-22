96457682e62ab31a042d10db16fec16f5f09dfe4
Siul <kksu149@naver.com>
2025-04-22 11:29:20 +0900

    모달창 페이지에 구현 완료

---
+++ my_web/my_web/web-study.js
@@ -0,0 +1,51 @@
+let step = 1;
+
+function openModal() {
+    document.getElementById("modal").classList.remove("hidden");
+    document.body.classList.add("overflow-hidden");
+    step = 0;
+    updateModalText();
+}
+
+function closeModal() {
+    document.getElementById("modal").classList.add("hidden");
+    document.body.classList.remove("overflow-hidden");
+}
+
+function nextStep() {
+    step++;  // 단계 진행
+    updateModalText();  // 텍스트 업데이트
+}
+
+function lastStep() {
+    step--;  // 단계 진행
+    updateModalText();  // 텍스트 업데이트
+}
+
+
+function updateModalText() {
+    const text = document.getElementById("modal-ContentText");
+    const last_btn = document.querySelector(".last-button");
+    const next_btn_text = document.querySelector(".next-button");
+
+    if (step == 0) {
+        text.textContent = "허헝~";
+        last_btn.style.display = "none";
+        next_btn_text.textContent = "다음으로";
+    }
+    else if (step === 1) {
+        text.textContent = "안녕하세융~~";
+        last_btn.style.display = "inline-block";
+    } else if (step === 2) {
+        text.textContent = "모달 창을 한 번 만들어봤어융";
+    } else if (step == 3) {
+        text.textContent = "쓸지 말지는 잘 모르겠으나 괜찮나용?";
+    } else if (step == 4) {
+        text.textContent = "그냥... 그렇다구용... 2주 가량 남았는데 파이팅 해보아용~";
+    } else if (step === 5) {
+        text.textContent = "A+를 향해서~ 빠잉";
+        next_btn_text.textContent = "끝내기";
+    } else {
+        closeModal();  // 마지막 단계 후 모달을 닫음
+    }
+}
